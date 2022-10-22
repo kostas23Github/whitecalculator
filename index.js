17d16efcde7d265f28793bcaa4695b6f978f9ac9
kostas <akwstas1@gmail.com>
2022-10-22 17:50:21 +0300

    Add history display. Fix percentage function.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // See NOTES.md for more info!
 
+
 //      **** DOM ELEMENTS ****
 const calculator = document.querySelector('.calculator-container');
 const screen = document.querySelector('.screen');
@@ -8,8 +9,14 @@ const wrapperStandard = document.querySelector('.wrapper-standard');
 const wrapperScientific = document.querySelector('.wrapper-scientific');
 const scientificBtn = document.querySelector('.mode');
 const standardBtn = document.querySelector('.mode:nth-child(2)');
-const lightBtn = document.querySelector('.mode:nth-child(3)');
-const darkBtn = document.querySelector('.mode:nth-child(4)');
+const lightBtn = document.querySelector('.mode:nth-child(5)');
+const darkBtn = document.querySelector('.mode:nth-child(6)');
+const back = document.querySelector('.mode:nth-child(3)');
+const forward = document.querySelector('.mode:nth-child(4');
+const historyText = document.querySelector('.history');
+const resultBtn = document.querySelectorAll('.result')[1];
+let beforeCalc = '';
+
 
 
 //      **** HANDLER FOR WHEN CLICKING A BUTTON ****
@@ -30,10 +37,11 @@ const addBtn = btn => {
             }
         } else if (nameOfClass.includes("result")) {
             try {
+                beforeCalc = display.value;
                 display.value = forEvaly(display.value);
             } catch (error) {
-                // display.value = 'Error';
-                console.log(error);
+                display.value = 'Error';
+                // console.log(error);
             }
         } else if (nameOfClass.includes("delete")) {
             display.value = '';
@@ -87,6 +95,71 @@ for (const btn of wrapperScientific.children) {
     btn.addEventListener('click', () => addBtn(btn));
 }
 
+//      **** HISTORY DISPLAY ****
+let historyArray = []; // Stores result inputs
+historyText.textContent = historyArray[0]; // Initiallizer
+let index = historyArray.indexOf(historyText.textContent); // Initiallizer. It is the index of where in the history array the history display value is.
+
+//      **** UPDATES-HISTORY LISTENER/FUNCTION ****
+resultBtn.addEventListener('click', () => {
+    // Whenever the result btn is clicked a new history text is pushed to the array
+    historyArray.push(`${beforeCalc} = ${display.value}`);
+    historyText.textContent = historyArray[historyArray.length - 1]; // If arrow btns aren't clicked or a new input is given the history text has the previous value of the current result
+    index = historyArray.indexOf(historyText.textContent); // Updates on new input
+    if (historyArray.length > 10) { // Stores max 10 inputs
+        historyArray.shift();
+    }
+    // When history text reaches ends of historyArray the history btns are hidden.
+    if (index === 0) {
+        back.style.visibility = 'hidden';
+    } else if (index === historyArray.length) {
+        forward.style.visibility = 'hidden';
+    } else {
+        back.style.visibility = 'visible';
+        forward.style.visibility = 'visible';
+    }
+
+});
+
+//      **** BACK HISTORY LISTENER/FUNCTION ****
+// Upon clicking it the previous history text is shown
+back.addEventListener('click', () => {
+    // console.log('back is clicked');
+    index = historyArray.indexOf(historyText.textContent);
+    console.log(index);
+    if (index > 0 && index <= historyArray.length - 1) {
+        historyText.textContent = historyArray[index - 1];
+    }
+    if (index === 0) {
+        back.style.visibility = 'hidden';
+    } else if (index === historyArray.length) {
+        forward.style.visibility = 'hidden';
+    } else {
+        back.style.visibility = 'visible';
+        forward.style.visibility = 'visible';
+    }
+});
+
+//      **** FORWARD HISTORY LISTENER/FUNCTION ****
+// Upon clicking it the next history text is shown
+forward.addEventListener('click', () => {
+    console.log('forward is clicked');
+    index = historyArray.indexOf(historyText.textContent);
+    console.log(index);
+    if (index < historyArray.length - 1 && index >= 0) {
+        historyText.textContent = historyArray[index + 1];
+    }
+    if (index === 0) {
+        back.style.visibility = 'hidden';
+    } else if (index === historyArray.length) {
+        forward.style.visibility = 'hidden';
+    } else {
+        back.style.visibility = 'visible';
+        forward.style.visibility = 'visible';
+    }
+})
+
+
 //      **** TOGGLE CALCULATOR ****
 const toggle = () => {
     const scientificBtnStyles = window.getComputedStyle(scientificBtn); // gets all the css styles for the specific element
@@ -97,9 +170,14 @@ const toggle = () => {
         wrapperStandard.style.display = 'grid';
         wrapperScientific.style.display = 'none';
         screen.style.height = '70px';
+        screen.style.display = 'flex';
+        screen.style.flexDirection = 'row';
         calculator.style.width = '300px';
         display.maxLength = '10';
         calculator.style.animation = '100ms startle 1 linear';
+        historyText.style.display = 'none';
+        back.style.display = 'none';
+        forward.style.display = 'none';
     } else if (scientificBtnStyles.display === 'flex') {
         standardBtn.style.display = 'flex';
         scientificBtn.style.display = 'none';
@@ -109,6 +187,11 @@ const toggle = () => {
         calculator.style.width = 'min-content';
         display.maxLength = '25';
         calculator.style.animation = '300ms rotate 1 reverse';
+        historyText.style.display = 'flex';
+        display.style.height = '50%';
+        screen.style.flexDirection = 'column-reverse';
+        back.style.display = 'flex';
+        forward.style.display = 'flex';
     }
 }
 
@@ -119,9 +202,9 @@ standardBtn.addEventListener('click', toggle);
 //      **** TOGGLE DARK-LIGHT MODE ****
 const toggleDarkMode = state => {
     const allElements = document.querySelector('body').querySelectorAll('*'); // select all body elements
-    switch(state) {
+    switch (state) {
         case 'dark':
-            allElements.forEach(element=> element.classList.add('dark')); // append dark class to every body > element
+            allElements.forEach(element => element.classList.add('dark')); // append dark class to every body > element
             // allElements.forEach(element=>console.log(element.classList)); // check line
             lightBtn.style.display = 'flex';
             darkBtn.style.display = 'none';
@@ -129,7 +212,7 @@ const toggleDarkMode = state => {
             state = 'light';
             break;
         case 'light':
-            allElements.forEach(element=> element.classList.remove('dark'));
+            allElements.forEach(element => element.classList.remove('dark'));
             // allElements.forEach(element=>console.log(element.classList)); // check line
             lightBtn.style.display = 'none';
             darkBtn.style.display = 'flex';
@@ -142,8 +225,8 @@ const toggleDarkMode = state => {
 }
 
 //      **** TOGGLE DARK-LIGHT MODE LISTENERS ****
-lightBtn.addEventListener('click', () => {toggleDarkMode('light')});
-darkBtn.addEventListener('click', () => {toggleDarkMode('dark')});
+lightBtn.addEventListener('click', () => { toggleDarkMode('light') });
+darkBtn.addEventListener('click', () => { toggleDarkMode('dark') });
 
 
 //      **** EVALY FUNCTIONS ****
@@ -169,7 +252,7 @@ const forEvaly = rawDisplayValue => {
     let middleValue;
     middleValue = rawDisplayValue.replaceAll('\u221A', 'sqrt');
     middleValue = factorialHandler(middleValue);
-    middleValue = percentageHandler(middleValue);
+    middleValue = percentage(middleValue);
     middleValue = evaly(middleValue).toString();
     if (middleValue.includes('.')) {
         if (middleValue.split('.')[1].length > 5) {
@@ -200,7 +283,7 @@ function factorial(n) {
     }
 }
 
- function factorialHandler(string) {
+function factorialHandler(string) {
     let arrStr = string.split(''); // Convert to array
     let openParenthesisIndex;
     let closingParenthesisIndex;
@@ -256,112 +339,105 @@ function factorial(n) {
 }
 
 
-//  **** % CALCULATOR ****
+//      **** % CALCULATOR ****
 // Percent(%) is calculated as the sum of all previous numbers and then the percentage of that sum e.g. 5 + 6% = 5.3, 5 + 5 + 6% = 10.6
 
-
-const percentageHandler = string => {
-    let array = string.split(''); // convert string to array
-    // let index = array.indexOf('%'); // the first instance of %
+const percentage = string => {
+    let array = string.split('');
+    // console.log(array);
     let percArray = [];
-    for (let indexx in array) {
-        if (array[indexx] === '%') {
-            percArray.push(indexx);
+    let from = 0;
+    array.forEach(element => {
+        // console.log(element);
+        if (element === '%') {
+            let index = array.indexOf(element, from);
+            percArray.push(index);
+            // has all indexes of '%'
+            from = index + 1;
+        }
+    });
+    // console.log(percArray);
+    let oldpercIndex = 0;
+    for (let percIndex of percArray) {
+        percIndex = percIndex - oldpercIndex; //1
+        // console.log(percIndex);
+        let subArray = array.slice(0, percIndex);
+        let result = percMath(subArray);
+        if (!result) {
+            display.value = 'If % inside (), calculate the value of () separetly.';
+            // console.log('error')
+            // return 1;
+        } else {
+            array.splice(0, percIndex + 1, result);
+            oldpercIndex = percIndex;
         }
     }
-    // Now percArray has all indexes of %
-    let indexesToCut = 0;
-    percArray.forEach(index => {
-        let cutAdjuster = indexesToCut; // with each iteration the array changes and 
-        let subArray = array.slice(0, index);
-        indexesToCut = subArray.length + 1 - cutAdjuster;
-        console.log(`IndexesToCut: ${indexesToCut}`);
-        if (subArray[index - 1] === ')') { // check for parenth
-            console.log(`( exists`)
-            let i = index - 1; // i is index of )
-            while (i >= 0 && subArray[i] !== '(') {
-                i--; // when while ends i is index of (
-            }
-            if (i === 0) {
-                console.log(`No calculations before (`)
-                let numberOfPerc = evaly(subArray.slice(i, index).join(''));
-                let perc = evaly(numberOfPerc / 100);
-                array.splice(0, indexesToCut, perc);
-                return array.join('');
-            } else {
-                console.log(`calculations before (`)
-                let j = i - 1; // j is the index of operator before (
-                let calculations = evaly(subArray.slice(0, j).join('')); // calculates up untill the number before (
-                let numberOfPerc = evaly(subArray.slice(i, index).join('')); // calculates the inside of parenthesis, which is the number before %
-                let perc;
-                switch (subArray[j]) {
-                    // In first 4 cases calculations exist prior to ( format=(5 @ ()% or 5 @ ()% @ 5)
-                    case '+':
-                        perc = evaly(calculations + calculations * numberOfPerc / 100);
-                        break;
-                    case '-':
-                        perc = evaly(calculations - calculations * numberOfPerc / 100);
-                        break;
-                    case '*':
-                        perc = evaly(calculations * numberOfPerc / 100);
-                        break;
-                    case '/':
-                        perc = evaly(calculations / numberOfPerc / 100);
-                        break;
-                    default: // nothing to return if reaches this casse probably an error or typo
-                        return 'error';
-                }
-                array.splice(0, indexesToCut, perc);
-                return array.join('');
-            }
-        } else { // no parenth
-            console.log(`no parenthesis before %`)
-            let i = index - 1;
-            while (i >= 0 && subArray[i] != '+' && subArray[i] != '-' && subArray[i] != '*' && subArray[i] != '/') {
-                i--;
-            }
-            console.log(`i: ${i}`);
-            let perc;
-            if (i === -1) {
-                console.log(`only a number`)
-                // only number format=(5% or 5% @ 5)
-                perc = evaly(subArray.slice(0, index).join('') / 100);
-                array.splice(0, indexesToCut, perc);
-            } else {
-                console.log('calculations before %')
-                console.log(subArray);
-                let calculations = evaly(subArray.slice(0, i).join(''));
-                console.log(`calculations: ${calculations}`)
-                let numberOfPerc = evaly(subArray.slice(i + 1, indexesToCut).join(''));
-                console.log(`numberOfPerc: ${numberOfPerc}`)
-                switch (subArray[i]) {
-                    case '+':
-                        perc = evaly(calculations + calculations * numberOfPerc / 100).toFixed(3);
-                        console.log(`perc: ${perc}`)
-                        break;
-                    case '-':
-                        perc = evaly(calculations - calculations * numberOfPerc / 100).toFixed(3);
-                        break;
-                    case '*':
-                        perc = evaly(calculations * numberOfPerc / 100).toFixed(3);
-                        break;
-                    case '/':
-                        perc = evaly(calculations / (numberOfPerc / 100)).toFixed(3);
-                        break;
-                    default:
-                        return 'error';
-
-                }
-                if (array[indexesToCut] === '%') {
-                    array.splice(0, indexesToCut + 1, perc);
-                } else {
-                    array.splice(0, indexesToCut, perc);
-                }
-            }
-            console.log(`perc: ${perc}`)
-            // array.splice(0, indexesToCut, perc); // cuts from the original array the subarray
-            console.log(`array: ${array}`)
-        }
-    })
     return array.join('');
+}
+const percMath = subArray => {
+    // let array = string.split('');
+    // let index = array.indexOf('%');
+    let i = subArray.length - 1;
+    // let number = /[0-9]/g;
+    let prior, just, percentage, flag, priorPar;
+    while (i >= 0 && !flag) {
+        switch (subArray[i]) {
+            case '+':
+                // console.log(`+ before`);
+                prior = evaly(subArray.slice(0, i).join(''));
+                // console.log(`prior: ${prior}`)
+                just = evaly(subArray.slice(i + 1).join(''));
+                // console.log(`just: ${just}`)
+                percentage = prior + prior * just / 100;
+                // console.log(`percentage: ${percentage}`)
+                flag = i;
+                break;
+            case '-':
+                // console.log(`- before`);
+                prior = evaly(subArray.slice(0, i).join(''));
+                // console.log(`prior: ${prior}`)
+                just = evaly(subArray.slice(i + 1).join(''));
+                // console.log(`just: ${just}`)
+                percentage = prior - prior * just / 100;
+                // console.log(`percentage: ${percentage}`)
+                flag = i;
+                break;
+            case '*':
+                // console.log(`* before`);
+                prior = evaly(subArray.slice(0, i).join(''));
+                // console.log(`prior: ${prior}`)
+                just = evaly(subArray.slice(i + 1).join(''));
+                // console.log(`just: ${just}`)
+                percentage = prior * just / 100;
+                // console.log(`percentage: ${percentage}`)
+                flag = i;
+                break;
+            case '/':
+                // console.log(`/ before`);
+                prior = evaly(subArray.slice(0, i).join(''));
+                // console.log(`prior: ${prior}`)
+                just = evaly(subArray.slice(i + 1).join(''));
+                // console.log(`just: ${just}`)
+                percentage = prior / (just / 100);
+                // console.log(`percentage: ${percentage}`)
+                flag = i;
+                break;
+            case '(':
+                // console.log(`( before`);
+                return undefined;
+            case ')':
+                // console.log(`() before`);
+                return undefined;
+        }
+        i--;
+    }
+    if (i === -1) {
+        console.log(`No calcs before`);
+        console.log(evaly(subArray.join('') / 100));
+        return percentage = evaly(subArray.join('') / 100);
+    } else {
+        console.log(subArray);
+        // return array.splice(0, index + 1, percentage).join('');
+        return percentage;
+    }
 }
